d9b4e262917544720ca1290bbddd98bb076d2c7f
iost-lv <lostlvcrypto@gmail.com>
2025-12-11 06:02:13 +0100

    Resolve merge conflicts

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = nextConfig
-
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
-
->>>>>>> 5ad9ba79b4c6621384a49d222b40cfb71747d02e
